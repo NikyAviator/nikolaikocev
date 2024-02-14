07d956dedc4cbbbb21754f712ddd85a3bf847fb8
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-02-14 14:25:16 +0100

    Changed the <p> in NikyTextInfo

--- frontend/src/Components/Accordion/NikyTextInfo.jsx
+++ frontend/src/Components/Accordion/NikyTextInfo.jsx
@@ -22,25 +22,30 @@ const NikyTextInfo = () => {
           <p>
             My professional journey has taken a fascinating turn; I began my
             career as a flight instructor, where I taught the art of navigating
-            the skies.
+            the skies. Through this experience, I developed a strong
+            appreciation for details, accuracy, and a continuous pursuit of
+            knowledge. These traits became foundational and very useful when I
+            decided to transition into web development.
           </p>
-          Through this experience, I developed a strong appreciation for
-          details, accuracy, and a continuous pursuit of knowledge. These traits
-          became foundational and very useful when I decided to transition into
-          web development. Throughout my journey in the realm of web
-          development, I discovered a deep passion for technology and system
-          administration, especially when it comes to Linux. I have spent
-          countless hours exploring Linux environments, which has strengthened
-          my understanding of how systems operate. This knowledge, combined with
-          my focus on JavaScript, has equipped me with the tools I need to build
-          robust, efficient, and user-friendly web solutions. My background as
-          an instructor has also given me unique insights into web development.
-          Just as in aviation, web development requires a perfect balance
-          between theory and practice, between the art of coding and the science
-          of technology. My passion for technology and JavaScript, combined with
-          my aviation experience, allows me to create solutions that are both
-          technically advanced and intuitive for users.
-          {/* Your story goes here */}
+          <p>
+            Throughout my journey in the realm of web development, I discovered
+            a a deep interest for technology, programming, and especially
+            GNU/Linux. I have spent countless hours exploring Linux
+            environments, which has strengthened my understanding of how systems
+            operate.
+          </p>
+          <p>
+            My experience in aviation has also given me a unique perspective on
+            problem-solving. In aviation, the ability to think on your feet and
+            solve problems quickly is essential. This skill has been invaluable
+            in my transition to web development, where I have been able to
+            quickly adapt to new technologies and solve complex problems.
+          </p>
+          <p>
+            This knowledge, combined with my focus on JavaScript and Java, has
+            equipped me with the tools I need to build robust, efficient, and
+            user-friendly web solutions.
+          </p>
         </Card.Text>
       </Card.Body>
     </Card>
