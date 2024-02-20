af0a3b66f0271836412a6b7910d5969329cc8c5e
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-02-20 17:01:26 +0100

    Dockerfile added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// default value is dist
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+  },
+});
