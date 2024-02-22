5064a310da8de3c43712c561af642727712aa12b
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-02-22 16:04:16 +0100

    V1.0.2! Acc exp added!

--- frontend/src/Components/Accordion/data.js
+++ frontend/src/Components/Accordion/data.js
@@ -2,31 +2,53 @@ const data = [
   {
     id: '1',
     question:
-      'Feb 2023- Aug 2023: Backend Developer - IKEA - Interaction Core ',
+      'Feb 2023 - Aug 2023: Backend Developer - IKEA - Interaction Core ',
     answer:
       'From February to April 2023, I interned at IKEA, during which I wrote a' +
       'thesis on Redis vs. Bigtable, deepening my grasp on team-utilized' +
       'technologies. Following this, I joined as a summer contractor from June to' +
       'August 2023. Alongside two colleagues, we developed a Bigtable API.' +
-      ' Tech: JS, Python (Locust), Redis & Bigtable, Bash, Docker, GCP (Google Cloud Platform)',
+      ' Tech: JavaScript, Python (Locust), Redis & Bigtable, Bash, Docker, GCP (Google Cloud Platform)',
   },
   {
     id: '2',
-    question: 'What are they used for?',
+    question: 'Apr 2022 - Jun 2022: Frontend Developer - ONE Nordic AB',
     answer:
-      'They are commonly employed in various contexts, including FAQs, product descriptions, navigation menus, settings panels, and data tables, to save screen space and provide a structured and user-friendly interface for presenting information or options.',
+      'During my first internship, I was tasked with improving the UX/UI of the company’s new website, making it more responsive using Bootstrap, integrating the Google Maps API, among other tasks. This experience enhanced my skills in C#, JavaScript, and Bootstrap, contributing to a more engaging user experience. Post-internship, I continued to assist in web development and design as an employee. Tech: C#, JavaScript, Bootstrap, Google Maps API.',
   },
   {
     id: '3',
-    question: 'Accordion as a musical instrument',
+    question: 'Jan 2021 - Aug 2021: Airport Analyst - Flygprestanda AB',
     answer:
-      'The accordion is a musical instrument with a keyboard and bellows. It produces sound by air passing over reeds when the player expands or compresses the bellows, used in various music genres.',
+      'My role involved updating an in-house database with new airport data, creating engine-out procedures for pilots, and utilizing GNU/Linux for all tasks. This position required a meticulous approach to data management and problem-solving, skills that I have carried forward in my software development career. Tech: GNU/Linux',
   },
   {
     id: '4',
-    question: 'Can I create an accordion component with a different framework?',
+    question:
+      'Oct 2019 - July 2020: Freelance First Officer - Royal Air, Bromma, Stockholm.',
     answer:
-      'Yes of course, it is very possible to create an accordion component with another framework.',
+      'Flew for a small private jet company (Cessna Citation CJ3). I was located ' +
+      'in Stockholm. When Covid-19 hit business slowed down. I moved back to' +
+      ' Skåne and continued to work as a Flight Instructor and simulator pilot.',
+  },
+  {
+    id: '5',
+    question:
+      'Jan 2018 - Aug 2021: Flight Instructor PPL - Kristianstads Flygklubb, Sweden.',
+    answer:
+      'Flight instructor, taught students to fly small airplanes (PA28) in Kristianstad,' +
+      ' Skåne. Also flew tour flights for people who wanted to experience flying and controlling the airplane.',
+  },
+  {
+    id: '6',
+    question:
+      'Jan 2017 - Dec 2020: Simulator Pilot - Entry Point North, Malmö, Sweden.',
+    answer:
+      'EPN offers a computer-based simulation that allows students training to ' +
+      'become air traffic controllers to practice controlling airplanes in a ' +
+      'controlled environment. This simulation provides a safe and effective way ' +
+      'for students to develop the necessary skills and experience before ' +
+      'entering the field. I was controlling the airplanes.',
   },
 ];
 
