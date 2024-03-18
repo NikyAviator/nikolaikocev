d9f178934a50317ce963f49862b92360f0275237
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-03-18 13:00:12 +0100

    npm update & started to work on Modal component.

--- frontend/src/Components/custom-modal/ModalTest.jsx
+++ frontend/src/Components/custom-modal/ModalTest.jsx
@@ -1,9 +1,19 @@
 import '../../scss/styles.scss';
 import { Button } from 'react-bootstrap';
+import { useState } from 'react';
+import Modal from './Modal';
+
 const ModalTest = () => {
+  const [showModalPopup, setShowModalPopup] = useState(false);
+
+  const handleToggleModalPopup = () => {
+    setShowModalPopup(!showModalPopup);
+  };
+
   return (
     <>
-      <Button>Open Modal Popup</Button>
+      <Button onClick={handleToggleModalPopup}>Open Modal Popup</Button>
+      {showModalPopup && <Modal />}
     </>
   );
 };
