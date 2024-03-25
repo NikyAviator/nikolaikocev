57815aed91daf7b14336808045f1ff660e604b8d
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-03-25 19:25:14 +0100

    FF mounted in TTTPage

--- frontend/src/Components/feature-flag/FeatureFlags.jsx
+++ frontend/src/Components/feature-flag/FeatureFlags.jsx
@@ -2,9 +2,9 @@ import '../../scss/styles.scss';
 
 const FeatureFlags = () => {
   return (
-    <div>
+    <>
       <h1>Feature Flags:</h1>
-    </div>
+    </>
   );
 };
 
--- frontend/src/Pages/TTTPage.jsx
+++ frontend/src/Pages/TTTPage.jsx
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import TicTacToe from '../Components/tic-tac-toe/TicTacToe';
+import FeatureFlags from '../Components/feature-flag/FeatureFlags';
 
 const TTTPage = () => {
   return (
@@ -11,6 +12,12 @@ const TTTPage = () => {
         <Col>
           <h1>Hello and welcome to: Tic Tac Toe (luffarschack)</h1>
           <TicTacToe />
+        </Col>
+      </Row>
+      <hr />
+      <Row>
+        <Col>
+          <FeatureFlags />
         </Col>
       </Row>
     </Container>
