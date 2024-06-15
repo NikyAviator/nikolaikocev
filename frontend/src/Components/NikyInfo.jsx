6ea145d6df7eb52fa6e8907a866370a9fb876ca9
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-06-15 21:19:47 +0200

    V1.1.1! New fresh picture!

--- frontend/src/Components/NikyInfo.jsx
+++ frontend/src/Components/NikyInfo.jsx
@@ -6,7 +6,7 @@ import Col from 'react-bootstrap/Col';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import nikyPic from '/pictures/niky.png';
+import nikyPic from '/pictures/Niko.jpg';
 import linux from '/pictures/linux.png';
 import html from '/pictures/html-5.png';
 import css from '/pictures/css-3.png';
