a6901ea13cd51f0a5196a8a4648d051b2213d54e
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-06-16 17:48:37 +0200

    Updated Footer.jsx

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -29,7 +29,7 @@ const Footer = () => {
             <FaLinkedin size='1em' />
           </a>
         </div>
-        <span className='mx-2'>V1.1.1 Last updated: 2024-06-15</span>
+        <span className='mx-2'>V1.1.1 Last updated: 2024-06-16</span>
       </div>
     </footer>
   );
