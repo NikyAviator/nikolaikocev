473663888db4608946946b237536a2afcdc48f7b
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-08-06 21:35:18 +0200

    Updated my work exp on front page

--- frontend/src/Components/Accordion/data.js
+++ frontend/src/Components/Accordion/data.js
@@ -1,6 +1,12 @@
 const data = [
   {
     id: '1',
+    question: 'June 2024 - Aug 2024: Backend Developer - IKEA - CDA',
+    answer:
+      'During my summer job, I collaborated with another summer worker to develop an Availability Test API. This API tests the entire data pipeline by sending custom payloads to simulate user interactions and purchases. It ensures data integrity and accuracy by validating that events are processed and stored correctly. The API supports custom payloads, enabling comprehensive and flexible testing scenarios to monitor the performance and availability of our data processing systems.',
+  },
+  {
+    id: '2',
     question:
       'Feb 2023 - Aug 2023: Backend Developer - IKEA - Interaction Core ',
     answer:
@@ -11,47 +17,47 @@ const data = [
       'colleagues on developing a Bigtable API.',
   },
   {
-    id: '2',
+    id: '3',
     question: 'Apr 2022 - Jun 2022: Frontend Developer - ONE Nordic AB',
     answer:
       "During my first internship, I focused on enhancing the UX/UI design of the company's new website, improving responsiveness with Bootstrap, and integrating the Google Maps API. Following the internship, I was employed for an extra month as a summer worker, further contributing to web development and design tasks. Tech used: C#, JS & Bootstrap.",
   },
   {
-    id: '3',
+    id: '4',
     question: 'Jan 2021 - Aug 2021: Airport Analyst - Flygprestanda AB',
     answer:
       'Updated an in-house database with new information (new runways, obstacles etc). Created engine-out procedures on request from customers. This is where my interest in GNU/Linux began. The company was using Debian and I was curious about it. My friend recommended my to install Arch Linux, and I was hooked. I have been using Arch Linux as my daily driver since then.',
   },
   {
-    id: '4',
+    id: '5',
     question:
       'Oct 2019 - July 2020: Freelance First Officer - Royal Air, Bromma, Stockholm.',
     answer:
       'Flew for a small private jet company (Cessna Citation CJ3). I was located in Stockholm.',
   },
   {
-    id: '5',
+    id: '6',
     question:
       'Mar 2018 - Mar 2021: Flight Instructor PPL - Eslövs Flygklubb, Sweden.',
     answer:
       'Flight instructor, taught students to fly small airplanes in Eslöv, Skåne. Also flew tour flights for people who wanted to try flying. We flew C172 and C152. Eslöv has a beatiful airfield. It also has a big aviation community that I really enjoy being a part of.',
   },
   {
-    id: '6',
+    id: '7',
     question:
       'Jan 2018 - Aug 2021: Flight Instructor PPL - Kristianstads Flygklubb, Sweden.',
     answer:
       'Flight instructor, taught students to fly small airplanes in Kristianstad, Skåne. Also flew tour flights for people who wanted to try flying. We flew PA-28. I really enjoy the aircraft in Kristianstad. It is a very nice place to have as a base. The runway is long and the airfield is very well maintained. The club has a lot of members and a lot of activities. I really enjoy being a part of this aviation club.',
   },
   {
-    id: '7',
+    id: '8',
     question:
       'Jan 2017 - Dec 2020: Simulator Pilot - Entry Point North, Malmö, Sweden.',
     answer:
       'At EPN, I operated a specialized software simulation for ATC student training, executing aircraft control commands issued by the trainees. This role involved managing virtual aircraft within the simulated environment, providing a realistic and safe training experience to develop their air traffic control skills.',
   },
   {
-    id: '8',
+    id: '9',
     question:
       'Jan 2014 - Dec 2016: Refugee Caregiver - Flintevångsgården, Staffanstorp, Sweden.',
     answer:
