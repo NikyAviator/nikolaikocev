06b86e36cb3fd9493050a2637df77d875d5a5038
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-11-25 14:06:21 +0100

    npm u + text u

--- frontend/src/Components/Accordion/NikyTextInfo.jsx
+++ frontend/src/Components/Accordion/NikyTextInfo.jsx
@@ -17,35 +17,27 @@ const NikyTextInfo = () => {
         <Card.Text className='sixtyfour-regular'>
           <p>Hey there!</p>
           <p>
-            I'm Nikolai Kocev, once a flight instructor, now a junior full-stack
-            developer diving into JavaScript and Java. My days up in the air
-            taught me a ton about precision and quick thinking—skills that come
-            in super handy in the tech world. Transitioning from teaching pilots
-            to coding might seem like a big leap, but it's all about tackling
-            challenges head-on, whether they're in the sky or on the screen.
+            I'm Nikolai Kocev, a pilot and flight instructor from Sweden who's
+            taken a dive into the world of coding. I’ve always loved flying—it’s
+            been a huge part of my life—but I wanted to try something new and
+            challenge myself in a completely different way.
           </p>
           <p>
-            My journey into tech kicked off when a friend reignited my interest
-            in Linux. They suggested that if I wanted to shift gears from being
-            a flight instructor to becoming a programmer, getting comfortable
-            with Linux was crucial. I began by tackling the installation of Arch
-            Linux—a challenge that consumed an entire day but ultimately was a
-            success. That day marked my transition; I switched my primary
-            workstation from Windows to Linux, although I still boot up Windows
-            for some gaming downtime.
+            A good friend of mine, who’s a programmer, nudged me toward the
+            world of IT. They said, "If you want to get into programming, start
+            by understanding GNU/Linux." So, I jumped right in with Arch Linux.
+            It wasn’t easy—setting up a stable system took hours of tinkering
+            and trial-and-error. But once it was up and running, I was hooked. I
+            loved the freedom to customize, experiment, and learn at every turn.
+            That one decision completely changed my perspective and opened the
+            door to a new passion.
           </p>
           <p>
-            This website serves a dual purpose: it's a learning tool and a
-            portfolio where I can demonstrate my growing skills in JavaScript,
-            React, Java, and other technologies. Currently, the site utilizes
-            React for the front end and is hosted on Cloudflare, thanks to
-            guidance and support from a good friend who helped me set it up.
-            This was another valuable learning opportunity for me. As I continue
-            to explore and learn, the website will evolve, reflecting new skills
-            and experiments with different technologies. I'm considering
-            expanding the site to include a JavaScript (NodeJS) backend,
-            potentially adding features like a blog or login system to practice
-            integrating both front-end and back-end development.
+            To turn this newfound interest into a career, I applied for a
+            two-year vocational programming course at Teknikhögskolan in Lund,
+            Sweden. It’s been an incredible journey so far, diving deep into
+            programming languages, frameworks, and building projects that blend
+            creativity and problem-solving.
           </p>
           <p>
             I am always open to making new connections and discussing anything
--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -29,7 +29,7 @@ const Footer = () => {
             <FaLinkedin size='1em' />
           </a>
         </div>
-        <span className='mx-2'>V1.1.4 Last updated: 2024-10-21</span>
+        <span className='mx-2'>V1.1.5 Last updated: 2024-11-25</span>
       </div>
     </footer>
   );
